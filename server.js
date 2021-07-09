bed89ac9c18176231f04d858a698599c83bcfb8a
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-09 17:06:10 -0500

    init commit

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 
 const PORT = "8000";
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("This is just the beginning..."));
 
 app.listen(PORT, console.log(`Listening on PORT: ${PORT}`));
